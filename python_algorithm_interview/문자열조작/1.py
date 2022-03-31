4cd142d0bcf7645dc9a9b64c599b49acd22194d3
dopa <62049139+dohyeonghan@users.noreply.github.com>
2022-03-31 20:26:33 +0900

    Create 1.py

---
+++ python_algorithm_interview/문자열조작/1.py
@@ -0,0 +1,15 @@
+class Solution:
+    # 반환값이 bool값
+    def isPalindrome(self, s: str) -> bool:
+        strs = [] 
+        for char in strs:
+            # 문자열이 알파벳([a-zA-Z])과 숫자([0-9])로만 구성되었는지 확인하는 메소드 
+            if char.isalnum():
+                # 소문자로 통일해서 strs 빈리스트에 차례대로 넣어준다.
+                strs.append(strs.lower())
+        
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False 
+        
+        return True
