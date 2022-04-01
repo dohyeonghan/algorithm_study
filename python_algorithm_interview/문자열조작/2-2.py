b68fe467f9219ec519ad23b3b715da34f68bf41a
dopa <62049139+dohyeonghan@users.noreply.github.com>
2022-04-01 14:03:59 +0900

    Create 2-2.py

---
+++ python_algorithm_interview/문자열조작/2-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        '''pythonic way.
+        reverse 함수는 리스트에만 적용 '''
+        s.reverse()
